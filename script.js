936ec83cbc621a361901deb2112189e68f1dbbd5
Deniz Hürmeydan <101830721+denizhurmeydan@users.noreply.github.com>
2023-03-31 01:09:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const myFirstName = "Deniz"
+const myLastName = "Hürmeydan"
+const myFullName = myFirstName + myLastName
+let myHobbies = ["biking", "camping", "DIY"]
+let currentStudent = false
+let myDream = "moving to america with my bf"
+const bestFriend = "Ekin"
+let phoneNumber = "05361231223"
+let favoriteMeal = "Carbonara"
+const myBirthYear = 1997
+let currentYear = 2023
+let myAge = currentYear - myBirthYear
+
+
+
